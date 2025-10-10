b2b9e3c5f1d07055f870122ada98cd0084af75f5
Karol Michał Chądzyński <105883691+Karolcha100@users.noreply.github.com>
2025-10-10 23:45:33 +0200

    Strategy Structure
    Strategy Structure Code

---
+++ Strategy/Reproduction/Energy/Energy.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+
+class EnergyTemplate(ABC):
+    @abstractmethod
+    def if_reproduce(self, current_value: float) -> bool:
+        pass
+
+    @abstractmethod
+    def get_energy_for_child(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_total_parent_energy_loss(self) -> float:
+        pass
+
+
+
+class FlatConditionEnergy(EnergyTemplate):
+    def __init__(
+            self, 
+            condition_value : float,
+            energy_for_child : float,
+            energy_additional_loss : float = 0.0,
+        ) -> None:
+
+        self.condition_value : float = condition_value
+        self.energy_for_child : float = energy_for_child
+        self.energy_additional_loss : float = energy_additional_loss
+
+    def if_reproduce(self, current_value : float) -> bool:
+        return current_value >= self.condition_value
+
+    def get_energy_for_child(self) -> float:
+        return self.energy_for_child
+    
+    def get_total_parent_energy_loss(self) -> float:
+        return self.energy_for_child + self.energy_additional_loss
+    
---
+++ Strategy/Strategy.py
@@ -0,0 +1,16 @@
+from typing import TypeVar, Generic, Type
+
+T = TypeVar("T", bound="Strategy")
+
+class Strategy:
+    def __init__(self, strategy_id: str, modules: dict[str, object]) -> None:
+        self.strategy_id = strategy_id
+        self.modules = modules
+        for name, module in modules.items():
+            setattr(self, name, module)
+
+    @classmethod
+    def create(cls: Type[T], strategy_id: str, **modules: object) -> T:
+        """Tworzy instancję i przypisuje moduły jako atrybuty"""
+        instance = cls(strategy_id, modules)
+        return instance
